9aa35334451b6db20718e63e7afdd5829fd18fa3
Aliy Djani <testo_house26@mail.ru>
2024-11-14 14:29:59 +0300

    Initial commit staticfiles

--- store/settings.py
+++ store/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "eShop/static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "eShop/static",
+]
 
-STATIC_ROOT = BASE_DIR / 'eShop/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
